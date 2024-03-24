184fd17e6d21447edc40ae80dfbb9dddd8d3b875
Adolfo Rosa <adolfoklk0307@gmail.com>
2024-03-24 09:19:49 -0400

    Starting a giff proyect with react + tailwind

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './app.css'
+function App () {
+  return (
+    <>
+      <form>
+        <label htmlFor='gif'>Buscar gif</label>
+        <input type='text' id='gif' name='gif' />
+      </form>
+    </>
+  )
+}
+
+export default App
