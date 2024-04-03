7fd771c534fbb319839f6c110554a1b70ef2e183
Adolfo Rosa <adolfoklk0307@gmail.com>
2024-04-03 18:20:02 -0400

    Several advances, list of TODO updated

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   resolve: {
-
     alias: {
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@public': '/public',
-      '@api': '/src/API',
-      '@config': '/config.js',
-      '@hooks': '/src/customHooks'
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@public': path.resolve(__dirname, './public'),
+      '@api': path.resolve(__dirname, './src/API'),
+      '@config': path.resolve(__dirname, './config.js'),
+      '@hooks': path.resolve(__dirname, './src/customHooks')
     }
   }
 })
